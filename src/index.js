116ffd0b59ef4acdd2d96a49fa9b6f35ab1b936f
saharamirian <45035127+saharamirian@users.noreply.github.com>
2019-01-16 23:49:31 +1100

    Redux Store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
 import  { App } from './App'
+import store from "./store"
+
 
 
 const appn = 
-<BrowserRouter> 
+<Provider store={store}> 
+    <BrowserRouter> 
         <App />
     </BrowserRouter>
+    </Provider>
 
 
 ReactDOM.render( 
