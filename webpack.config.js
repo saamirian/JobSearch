130273088596f43a959a04cfc30add3ec681dab2
saharamirian <45035127+saharamirian@users.noreply.github.com>
2019-01-16 14:13:21 +1100

    install webpack & babel

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+
+const path = require('path');
+ 
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+        
+    },
+    module: {
+        rules: [{
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }, {
+          test: /\.css$/,
+          use:[
+            'style-loader',
+            'css-loader'
+          ]
+        },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      }
+      ],
+        
+        
+      },
+      
+      devtool: 'cheap-module-eval-source-map',
+      devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
+      },
+      devServer: {
+        port: 9000,
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
+      }
+      
+      
+};
