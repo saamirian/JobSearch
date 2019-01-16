ad3e706c0cd7fbdab7e269e3df6de82f8e89cbd4
saharamirian <45035127+saharamirian@users.noreply.github.com>
2019-01-17 00:29:14 +1100

    getting job array from from store and return location search

--- src/pages/searchresult.js
+++ src/pages/searchresult.js
@@ -1,11 +1,34 @@
 
 import React from "react";
+import { connect } from "react-redux";
+import JobsArray from '../components/jobs-array-map'
+import { loadResult } from  "../actioncreators/getresult"
 
-export default class Search extends React.Component {
+class Search extends React.Component {
     render (){
-        return (
-            <div>hii</div>
-        )
+        if (this.props.loadResult.length > 0) {
+            return (
+       
+            <div className="search-params">
+            <div><h1>Job search result for {this.props.location}</h1></div>
+             
+              <div><JobsArray jobs={this.props.loadResult} /></div>
+            </div>
+     );
     }
-
-}
+  
+    
+      return <div>There is no  job opportunity in {this.props.location}</div>
+    
+    }
+  }
+  const mapStateToProps = ({ location , loadResult }) => ({
+    location,
+    loadResult
+    
+  });
+  
+  
+    
+   
+    export default connect(mapStateToProps, loadResult)(Search);
